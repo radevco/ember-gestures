d2fc1b85d74e0f4b3f787a4bd7772ca3a4241894
Travis Hoover <thoov7@gmail.com>
2017-01-31 13:13:19 -0800

    Fixing instance-initializers

--- app/instance-initializers/ember-gestures.js
+++ app/instance-initializers/ember-gestures.js
@@ -6,11 +6,6 @@ export default {
   name: 'ember-gestures',
 
   initialize: function (instance) {
-    if (typeof(instance.lookup) === "function") {
-      instance.lookup('service:-gestures');
-    } else {
-      // This can be removed when we no-longer support ember 1.12 and 1.13
-      getOwner(instance).lookup('service:-gestures');
-    }
+    getOwner(instance).lookup('service:-gestures');
   }
 };
